457dbf0c5851d02b6289498c42baedecc05ae688
AndresLince <lynxdarmatron@gmail.com>
2024-01-28 15:00:13 -0500

    Fix typo

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -1,12 +1,12 @@
 import 'package:teslo_shop/features/shared/shared.dart';
 
-class LoginFormStage {
+class LoginFormState {
   final bool isPosting;
   final bool isFormPosted;
   final bool isValid;
   final Email email;
   final Password password;
-  LoginFormStage({
+  LoginFormState({
     this.isPosting = false,
     this.isFormPosted = false,
     this.isValid = false,
