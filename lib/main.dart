0c006ff7cac9cbebfa96a65e2b72f0ee888a8c74
AndresLince <lynxdarmatron@gmail.com>
2024-01-31 21:08:01 -0500

    Add initEnvironment on main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/config/config.dart';
-import 'package:teslo_shop/config/router/app_router.dart';
 
-void main() {
+void main() async {
+  await Environment.initEnvironment();
   runApp(
     // To install Riverpod, we need to add this widget above everything else.
     // This should not be inside "MyApp" but as direct parameter to "runApp".
