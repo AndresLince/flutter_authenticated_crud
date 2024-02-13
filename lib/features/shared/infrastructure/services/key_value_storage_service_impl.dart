3a261ec8693835b5d91ac07ad4a461731c388d83
AndresLince <lynxdarmatron@gmail.com>
2024-02-12 20:45:28 -0500

    Add getSharedPrefs function to KeyValueStorageServiceImpl

--- lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
+++ lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
@@ -1,6 +1,12 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'key_value_storage_service.dart';
 
 class KeyValueStorageServiceImpl extends KeyValueStorageService {
+  Future getSharedPrefs() async {
+    return await SharedPreferences.getInstance();
+  }
+
   @override
   Future<T?> getValue<T>(String key) {
     // TODO: implement getValue
