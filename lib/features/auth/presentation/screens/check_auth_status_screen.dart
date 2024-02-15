213070a42c45e6be154fa08d9bb0ff181d99519d
AndresLince <lynxdarmatron@gmail.com>
2024-02-15 17:24:01 -0500

    Add CheckAuthStatusScreen

--- lib/features/auth/presentation/screens/check_auth_status_screen.dart
+++ lib/features/auth/presentation/screens/check_auth_status_screen.dart
@@ -5,6 +5,10 @@ class CheckAuthStatusScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(strokeWidth: 2,),
+      ),
+    );
   }
 }
