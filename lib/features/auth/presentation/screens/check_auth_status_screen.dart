70678b1437bd52c17ecea139741db4dc3b3520b7
AndresLince <lynxdarmatron@gmail.com>
2024-02-15 17:40:10 -0500

    Add redirection on CheckAuthStatusScreen

--- lib/features/auth/presentation/screens/check_auth_status_screen.dart
+++ lib/features/auth/presentation/screens/check_auth_status_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
+import 'package:go_router/go_router.dart';
 
-class CheckAuthStatusScreen extends StatelessWidget {
+class CheckAuthStatusScreen extends ConsumerWidget {
   const CheckAuthStatusScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(authProvider, (previous, next) {
+      context.go('/');
+    });
     return const Scaffold(
       body: Center(
         child: CircularProgressIndicator(strokeWidth: 2,),
