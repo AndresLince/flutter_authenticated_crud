6326c0a2251f03fafe0a475de702114b1e789218
AndresLince <lynxdarmatron@gmail.com>
2024-02-16 17:44:00 -0500

    Create goRouterNotifierProvider

--- lib/config/router/app_router_notifier.dart
+++ lib/config/router/app_router_notifier.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
+
+final goRouterNotifierProvider = Provider((ref) {
+  final authNotifier = ref.read(authProvider.notifier);
+  return GoRouterNotifier(authNotifier);
+});
 
 class GoRouterNotifier extends ChangeNotifier {
   final AuthNofifier _authNofifier;
