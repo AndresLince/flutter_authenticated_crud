2596cc81d4c153c383f42407321cfeaf49a3bcd5
AndresLince <lynxdarmatron@gmail.com>
2024-02-16 17:48:51 -0500

    Remove unnecesary import

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/config/router/app_router_notifier.dart';
@@ -10,7 +8,7 @@ final goRouterProvider = Provider((ref) {
 
   final goRouterNotifier = ref.read(goRouterNotifierProvider);
   return GoRouter(
-    initialLocation: '/splash',
+    initialLocation: '/login',
     refreshListenable: goRouterNotifier,
     routes: [
       ///* Primera pantalla
